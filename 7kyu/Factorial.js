01e8753b8f0541b959eb85abaa524ebb9b654697
Pavel <sosnovskiy.pavel@inbox.ru>
2021-10-23 21:27:23 +0300

    Update Factorial.js

--- 7kyu/Factorial.js
+++ 7kyu/Factorial.js
@@ -7,7 +7,7 @@ More details about factorial can be found here.
 
 */
 
-
+//var1
 function factorial(n)
 {
   if(n < 0 || n > 12){
@@ -15,4 +15,18 @@ function factorial(n)
   }else {
     return (n !=1 && n != 0) ? (n * factorial(n-1)): 1;
     }
+}
+
+//var2
+function factorial(n)
+{
+  if (n < 0 || n > 12) {
+    throw new RangeError("The argument must be between 1 and 12.")
+  }
+  
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
 }
