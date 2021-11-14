7cd60521de7d3cc3aac4a55fff30252ba6f63189
Pavel <sosnovskiy.pavel@inbox.ru>
2021-11-14 22:22:28 +0300

    Update BuildASquare.js

--- 7kyu/BuildASquare.js
+++ 7kyu/BuildASquare.js
@@ -11,21 +11,26 @@ n = 3, so I expect a 3x3 square back just like below as a string:
 */
 
 //var 1
-function generateShape(integer){
-          let s = [];
-           let x = [];
-            
-           for ( let i = 0 ; i < integer; i++){
-             s.push('+');
-           }
-            
-           let y = s.join('');
-            
-           for ( let i= 0; i < s.length; i++){
-             x.push(y);
-           }
-         
-           return x.join('\n')
-         }
+function generateShape(integer) {
+    let s = [];
+    let x = [];
+
+    for (let i = 0; i < integer; i++) {
+        s.push('+');
+    }
+
+    let y = s.join('');
+
+    for (let i = 0; i < s.length; i++) {
+        x.push(y);
+    }
+
+    return x.join('\n')
+}
 
 
+//var 2
+
+function generateShape(integer) {
+    return Array(integer).fill('+'.repeat(integer)).join('\n')
+}
