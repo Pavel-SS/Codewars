275ece8d3eb50edcacba16f4473dc32300910092
Pavel <sosnovskiy.pavel@inbox.ru>
2022-02-22 20:47:04 +0300

    Create ConvertABooleanToAString.js

---
+++ 8kyu/ConvertABooleanToAString.js
@@ -0,0 +1,15 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//var1
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
+
+//var2
+
+function booleanToString(b){
+    return String(b);
+  }
