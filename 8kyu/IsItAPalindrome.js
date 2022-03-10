5785e5c3d4daa6ca42b259ff785ad7caaf69ca8e
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-10 22:32:56 +0300

    Create IsItAPalindrome.js

---
+++ 8kyu/IsItAPalindrome.js
@@ -0,0 +1,7 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+function isPalindrome(x) {
+    let originalWord = x.toLowerCase(),
+        reverseWord = x.toLowerCase().split('').reverse().join('')
+    return originalWord === reverseWord
+  }
