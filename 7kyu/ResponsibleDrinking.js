4e3543e41ba1a11ae207b5e8743222fb00992e84
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-18 22:15:48 +0300

    Update ResponsibleDrinking.js

--- 7kyu/ResponsibleDrinking.js
+++ 7kyu/ResponsibleDrinking.js
@@ -26,3 +26,6 @@ function hydrate(s) {
       return (`${glassWater} glasses of water`)
     }
   }
+
+  //var 2
+  const hydrate = (s, w = [...s].filter(x => !isNaN(x)).reduce((a, b) => a + +b, 0)) => `${w} glass${w === 1 ? '' : 'es'} of water`;
