c7acc5e3aaf2f6a8f162653a3a58134d6a6a15a3
Pavel <sosnovskiy.pavel@inbox.ru>
2022-06-10 23:56:27 +0300

    Update StringEndWith.js

--- 7kyu/StringEndWith.js
+++ 7kyu/StringEndWith.js
@@ -4,3 +4,14 @@
 
 // solution('abc', 'bc') // returns true
 // solution('abc', 'd') // returns false
+function solution(str, ending){
+    const str2 = str.split('')
+    let arr = []
+     for(let i = str.length; i <= str.length && i > ending.length; i--){
+      arr.unshift(str2[i])
+     }
+     arr.join('') == ending ? console.log(arr.join('') + " " +ending) : console.log(arr.join('') + " " +ending) 
+  }
+  solution('gffkffe', 'ffe')
+  // solution('abcde', 'abc')
+  solution('abcde', 'cde')
