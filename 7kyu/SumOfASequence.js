75914909e3a8bc4de27200bd4b5e71e67f895b15
Pavel <sosnovskiy.pavel@inbox.ru>
2022-06-13 23:23:20 +0300

    Create SumOfASequence.js

---
+++ 7kyu/SumOfASequence.js
@@ -0,0 +1,19 @@
+//Your task is to make function, which returns the sum of a sequence of integers.
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+// If begin value is greater than the end, function should returns 0
+
+
+//var 1
+const sequenceSum = (begin, end, step) => {
+    let arr = [];
+    if(begin <= end){
+       for ( let i = begin; i <=end; i+=step){
+      arr.push(i)
+    }
+      return arr.reduce((a,b)=> a+b)
+    }else{
+      return 0
+    }
+  };
+
+  
