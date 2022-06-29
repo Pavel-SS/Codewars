36001bcc3c3d774522ba31275cb919af91d451cb
Pavel <sosnovskiy.pavel@inbox.ru>
2022-06-29 22:45:10 +0300

    Update SpeedControl.js

--- 7kyu/SpeedControl.js
+++ 7kyu/SpeedControl.js
@@ -28,3 +28,6 @@ function gps(s, x) {
    }
    return Math.floor(Math.max(...arr))
 }
+
+//var 2
+const gps = (s, x) => Math.floor(3600 * x.slice(1).reduce((m, d, i) => Math.max(m, d - x[i]), 0) / s);
